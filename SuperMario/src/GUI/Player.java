3c788b408f67a7a5edfb9480610b892f912fd1c9
laje-E <136092273+laje-E@users.noreply.github.com>
2025-09-09 18:32:20 -0300

    se agrego movimiento y colisiones (hay que seguir con estas)

--- SuperMario/src/GUI/Player.java
+++ SuperMario/src/GUI/Player.java
@@ -1,5 +1,70 @@
-package GUI;
+	package GUI;
 
-public class Player {
+	import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+	public class Player extends JPanel {
+
+		private static final long serialVersionUID = 1L;
+		
+		public Player(int posX, int posY, int ancho, int alto) {
+			setBounds(posX, posY, ancho, alto);
+		}
+		
+		public void moverDerecha(int anchoPanel) {
+			int posX = getX();
+			int posY = getY();
+
+			if (posX + getWidth() < anchoPanel) {
+				setLocation(posX + 15, posY);
+			}
+		}
+		
+		public void moverIzquierda() {
+			int posX = getX();
+			int posY = getY();
+			
+			if (posX > 0) {
+				setLocation(posX - 15, posY);
+			}
+		}
+
+		public void saltar() {
+			int altura_limite = 100;
+			int velocidad_salto = 5;
+			
+			Timer timer = new Timer(30, new ActionListener() {
+				int alturaActual = 0;
+				boolean sube = true;
+				
+				public void actionPerformed(ActionEvent e) {
+					int posX = getX();
+					int posY = getY();
+					
+					
+					if (sube == true) { // Subiendo
+		                if (alturaActual < altura_limite) {
+		                    setLocation(posX, posY - velocidad_salto);
+		                    alturaActual += velocidad_salto;
+		                } else {
+		                    sube = false; // Cambia a bajada
+		                }
+		            } else { // Bajando
+		                if (alturaActual > 0) {
+		                    setLocation(posX, posY + velocidad_salto);
+		                    alturaActual -= velocidad_salto;
+		                } else {
+		                    ((Timer) e.getSource()).stop(); // Termina el salto
+		                }
+		            }
+				}
+			});
+			timer.start();
+			
+		}
+
+	
 }
--- SuperMario/src/GUI/SuperMario.java
+++ SuperMario/src/GUI/SuperMario.java
@@ -1,5 +1,85 @@
 package GUI;
 
-public class SuperMario {
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SuperMario extends JFrame{
+	private JPanel contentPane;	
+	private boolean aPressed = false;
+	private boolean dPressed = false;
+	private boolean wPressed = false;
+	
+	public SuperMario(){
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setFocusable(true);
+		requestFocusInWindow();
+		setBounds(100, 100, 816, 572);
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	
+		Player player = new Player(360, 470, 80, 10);
+		player.setBackground(Color.red);
+		contentPane.add(player);
+		
+		Obstaculo obstaculo = new Obstaculo(100, 470, 80, 10);
+		obstaculo.setBackground(Color.GREEN);
+		contentPane.add(obstaculo);
+		
+
+		
+		addKeyListener(new KeyListener() { // Se abre el listener para poder escuchar input del teclado en el juego.
+			
+			public void keyTyped(KeyEvent e) {} 
+
+		    public void keyPressed(KeyEvent e) {
+		        int teclaPresionada = e.getKeyCode();
+		        
+		        if (teclaPresionada == KeyEvent.VK_A) { aPressed = true; }      // Actualiza los booleanos de las teclas cuando están presionadas.
+		        if (teclaPresionada == KeyEvent.VK_D) { dPressed = true; }
+		        if (teclaPresionada == KeyEvent.VK_W) { wPressed = true; }
+
+		        if (aPressed) { 
+		        	if (player.getBounds().intersects(obstaculo.getBounds()))
+		        		/*alternativa = player.getX() <= obstaculo.getX() + obstaculo.getWidth() && player.getX() + getWidth() >= obstaculo.getX() &&
+		        						player.getY() <= obstaculo.getY() + obstaculo.getHeight() && player.getY() + getHeight() >= obstaculo.getY()*/
+		        	{
+		        		player.moverDerecha(contentPane.getWidth());
+		        	}
+		        	else {
+		        		player.moverIzquierda(); 
+		        	}
+		        	}
+		        if (dPressed) { 
+		        	if (player.getBounds().intersects(obstaculo.getBounds())) {
+		        		player.moverIzquierda(); 
+		        	}
+		        	else {
+		        		player.moverDerecha(contentPane.getWidth());
+		        	}
+		        }
+		        if (wPressed) { player.saltar(); } 
+		        
+		    }
+
+		    @Override
+		    public void keyReleased(KeyEvent e) {
+		        int teclaPresionada = e.getKeyCode();
+		        if (teclaPresionada == KeyEvent.VK_A) { aPressed = false; }     // Actualiza los booleanos cuando una tecla es soltada.
+		        if (teclaPresionada == KeyEvent.VK_D) { dPressed = false; }
+		        if (teclaPresionada == KeyEvent.VK_W) { wPressed = false; }
+		    }
+		});
+	}
+
+	public static void main(String[] args) {
+	    SuperMario juego = new SuperMario();
+	    juego.setVisible(true);
+	}
 }
