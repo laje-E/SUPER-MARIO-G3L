d3f7792eb70267f2400b7df6e9f05b8be6769b77
laje-E <136092273+laje-E@users.noreply.github.com>
2025-09-18 10:23:27 -0300

    se agrego el mensaje al final del nivel.

--- SUPER-MARIO-G3L-main/SuperMario/src/GUI/Obstaculo.java
+++ SUPER-MARIO-G3L-main/SuperMario/src/GUI/Obstaculo.java
@@ -1,12 +1,31 @@
 package GUI;
 
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Obstaculo extends JPanel{
+public class Obstaculo extends JPanel {
+    
+	private static final long serialVersionUID = 1L;
 	
-	public Obstaculo(int posX, int posY, int ancho, int alto) {
-		setBounds(posX, posY, ancho, alto);
-	}
-	
-	
+    private JLabel imagenLabel;
+    
+    public boolean traspasable = true;
+
+    public Obstaculo(ArrayList<Obstaculo> obstaculos, ImageIcon icon, boolean traspasable) {
+        this.setLayout(null);
+        if (icon != null) {
+            imagenLabel = new JLabel(icon);
+            imagenLabel.setBounds(0, 0, icon.getIconWidth(), icon.getIconHeight());
+            this.add(imagenLabel);
+            traspasable = this.traspasable;
+        }
+    }
+
+    public Obstaculo(ArrayList<Obstaculo> obstaculos) {
+        this(obstaculos, null, true); // constructor sin imagen
+  	}
+
 }
