ae45bc4cbc0951f08496bd557d720d6bd3eaa82c
Eluney Laje <eluneylaje@gmail.com>
2025-09-21 17:58:22 -0300

    se agrego lo que hizo sebas laflito

--- SuperMario/src/GUI/SuperMario.java
+++ SuperMario/src/GUI/SuperMario.java
@@ -9,7 +9,7 @@ public class SuperMario extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-        Nivel1 juego = new Nivel1();
+        Nivel3 juego = new Nivel3();
         juego.setVisible(true);
     }
     
