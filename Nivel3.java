496643bafbe96ed8ad7e22b7a7c1892ef50cc11c
gimeneznacho07 <136719918+gimeneznacho07@users.noreply.github.com>
2025-09-22 01:51:03 -0300

    YA ESTAN LOS NIVELES (hay detalles)

---
+++ Nivel3.java
@@ -0,0 +1,116 @@
+package GUI;
+
+import javax.swing.ImageIcon;
+
+public class Nivel3 extends NivelBase {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void construirNivel() {
+    	Sonido.reproducirMusicaLoop("sonidos/temasInGame/tema3/tema3.wav");
+
+    	ImageIcon maradonaIcon = new ImageIcon(getClass().getResource("/img/personajes/enemigos/yaEscalados/MaradonaDer.png"));
+        Enemigo Maradona = new Enemigo(700, 391, 48, 45, 400, 700, maradonaIcon, true, 30, this, true);
+        contentPane.add(Maradona);
+        enemigos.add(Maradona);
+        Maradona.empezarADisparar(player);
+        Maradona.movimientoJefe2(player);
+        
+       
+        agregarPiso();
+
+       
+        agregarPlataformasHorizontales();
+
+      
+        agregarObstaculosVerticales();
+    }
+
+    private void agregarPiso() {
+        ImageIcon metalIcon = new ImageIcon(getClass().getResource("/img/pisos/metal.png"));
+        ImageIcon ladrilloPiedraIcon = new ImageIcon(getClass().getResource("/img/pisos/ladrilloPiedra.png"));
+
+        int sueloY = 500;
+        int alturaVentana = 600;
+        int tileSize = 64;
+        int tilesX = anchoMapa / tileSize;
+        int tilesAbajo = (alturaVentana - sueloY) / tileSize;
+
+        // Piso superior
+        for (int i = 0; i < tilesX; i++) {
+            Obstaculo sueloMetal = new Obstaculo(obstaculos, metalIcon, false);
+            sueloMetal.setBounds(i * tileSize, sueloY - tileSize, tileSize, tileSize);
+            contentPane.add(sueloMetal);
+            obstaculos.add(sueloMetal);
+        }
+
+      
+        for (int y = 0; y < tilesAbajo; y++) {
+            for (int i = 0; i < tilesX; i++) {
+                Obstaculo ladrilloPiedra = new Obstaculo(obstaculos, ladrilloPiedraIcon, false);
+                ladrilloPiedra.setBounds(i * tileSize, sueloY + y * tileSize, tileSize, tileSize);
+                contentPane.add(ladrilloPiedra);
+                obstaculos.add(ladrilloPiedra);
+            }
+        }
+    }
+
+    private void agregarPlataformasHorizontales() {
+        ImageIcon ladrilloIcon = new ImageIcon(getClass().getResource("/img/pisos/ladrillo.png"));
+        ImageIcon metalIcon = new ImageIcon(getClass().getResource("/img/pisos/metal.png"));
+
+ 
+        int[][] plataformas = {
+            {180, 380, 120, 16},   // plataforma baja
+            {400, 320, 100, 16},
+            {650, 280, 120, 16},
+            {800, 350, 100, 16},
+            {1200, 300, 140, 16},
+            {1500, 400, 120, 16},
+            {1750, 360, 100, 16},
+            {2000, 310, 140, 16},
+            {2250, 380, 120, 16},
+            {2500, 340, 100, 16}
+        };
+
+        for (int[] p : plataformas) {
+            Obstaculo plat;
+            if (p[3] <= 32) {
+                plat = new Obstaculo(obstaculos, ladrilloIcon, false); //ladrillo
+            } else {
+                plat = new Obstaculo(obstaculos, metalIcon, true); //  metal
+            }
+            plat.setBounds(p[0], p[1], p[2], p[3]);
+            contentPane.add(plat);
+            obstaculos.add(plat);
+        }
+    }
+
+    private void agregarObstaculosVerticales() {
+        ImageIcon ladrilloIcon = new ImageIcon(getClass().getResource("/img/pisos/ladrillo.png"));
+        
+        int[][] columnas = {
+            {600, 380, 32, 100}, {1000, 370, 32, 120}, {1400, 390, 32, 90},
+            {1800, 360, 32, 130}, {2200, 380, 32, 110}, {2600, 400, 32, 100}
+        };
+
+        for (int[] c : columnas) {
+            Obstaculo col = new Obstaculo(obstaculos, ladrilloIcon, false);
+            col.setBounds(c[0], c[1], c[2], c[3]);
+            contentPane.add(col);
+            obstaculos.add(col);
+        }
+    }
+
+    @Override
+    protected int getNumeroNivel() {
+        return 3;
+    }
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            Nivel1 nivel1 = new Nivel1();
+            nivel1.setVisible(true);
+        });
+    }
+}
---
+++ NivelBase.java
@@ -0,0 +1,314 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public abstract class NivelBase extends JFrame {	
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// Abstract señala que esta clase es un constructor y que no puede crear nada por si solo, sino que es una base
+	protected JPanel contentPane;
+    protected Player player;
+    protected ArrayList<Obstaculo> obstaculos = new ArrayList<>();
+    protected ArrayList<Enemigo> enemigos = new ArrayList<>();
+    protected FondoPanel fondoPanel;
+    protected int worldOffset = 0;
+    protected boolean nivelSuperado = false;
+    protected int anchoMapa = 3200;
+    boolean aPressed = false;
+	boolean dPressed = false;
+	boolean wPressed = false;
+	protected Timer movimientoFluido;
+	private int iteracion = 1;
+	public ImageIcon mario;
+	protected Puntaje puntaje;
+
+
+
+    public NivelBase() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setBounds(100, 100, 800, 600);
+        contentPane = new JPanel(null);
+        setContentPane(contentPane);
+        contentPane.setFocusable(true);
+        contentPane.setFocusTraversalKeysEnabled(false);
+        
+     // inicializo hud
+        puntaje = new Puntaje(1, 300);
+        puntaje.setBounds(220, 10, 400, 30);
+        contentPane.add(puntaje);
+        
+        puntaje.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 15)); // cambia el tamaño de la fuente
+        puntaje.setForeground(java.awt.Color.BLACK); // cambia el color de la fuente
+        
+        configurarJugador();
+        construirNivel(); // método abstracto
+        configurarFondo();
+        configurarMovimiento();
+    }
+    
+
+    protected abstract void construirNivel(); // cada nivel define sus obstáculos y enemigos
+
+    private ArrayList<Bala> balas = new ArrayList<>();
+
+    public void agregarBala(Bala b) {
+        balas.add(b);
+    }
+    public void eliminarBala(Bala b) {
+        balas.remove(b);
+    }
+    public ArrayList<Bala> getBalas() {
+        return new ArrayList<>(balas); // copia defensiva
+    }
+    
+    protected void configurarJugador() {
+    	   URL url = getClass().getResource("/img/personajes/marioParadopng.png");
+    	   if (url != null) {
+    		   mario = new ImageIcon(url);
+    	   } else {
+    		   System.err.println("No se encontró la imagen: marioParadopng.png");
+  	  }
+
+        player = new Player(100, 350, 30, 50, obstaculos, enemigos, this, balas, mario);
+        player.setBackground(Color.RED);
+        player.setFocusable(false);
+        contentPane.add(player);
+    }
+
+    protected void configurarFondo() {
+        int numeroNivel = getNumeroNivel();  // <- método que cada subnivel implementa
+        fondoPanel = new FondoPanel(numeroNivel);
+        fondoPanel.setBounds(0, 0, anchoMapa, 600);
+        contentPane.add(fondoPanel);
+    }
+
+    protected abstract int getNumeroNivel();
+    
+    protected void configurarMovimiento() {
+    	
+    	// Pide mantener bien el foco para cuando la ventana se abra
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                contentPane.requestFocusInWindow();
+            }
+        });
+        
+        
+        contentPane.addKeyListener(new java.awt.event.KeyListener() {
+            @Override
+            public void keyTyped(java.awt.event.KeyEvent e) {}
+
+            @Override
+            public void keyPressed(java.awt.event.KeyEvent e) {
+            	if (nivelSuperado) return;
+            	
+                int tecla = e.getKeyCode();
+                if (tecla == java.awt.event.KeyEvent.VK_A) aPressed = true;
+                if (tecla == java.awt.event.KeyEvent.VK_D) dPressed = true;
+                if (tecla == java.awt.event.KeyEvent.VK_W) wPressed = true;
+            }
+
+            @Override
+            public void keyReleased(java.awt.event.KeyEvent e) {
+            	if (nivelSuperado) return;
+            	
+                int tecla = e.getKeyCode();
+                if (tecla == java.awt.event.KeyEvent.VK_A) aPressed = false;
+                if (tecla == java.awt.event.KeyEvent.VK_D) dPressed = false;
+                if (tecla == java.awt.event.KeyEvent.VK_W) wPressed = false;
+            }
+        });
+        
+        
+
+        movimientoFluido = new Timer(15, new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+
+            	if (player != null && player.estaMuerto()) {
+            	    return; // no mover al jugador si está muerto
+            	}
+            	
+            	iteracion *= -1;
+            	
+            	if (aPressed) {
+            		if (iteracion == 1) {
+            			URL url = getClass().getResource("/img/personajes/MARIO_corriendo6.png");
+            			if (url != null) {
+            			    player.setIcon(new ImageIcon(url));
+            			} else {
+            			    System.err.println("No se encontró la imagen: MARIO_corriendo6.png");
+            			}
+            		}
+            		if (iteracion != 1) {
+            			URL url = getClass().getResource("/img/personajes/mario_corriendo7.png");
+            			if (url != null) {
+            			    player.setIcon(new ImageIcon(url));
+            			} else {
+            			    System.err.println("No se encontró la imagen: mario_corriendo7.png");
+            			}
+            		}
+            		
+            	    boolean puede_mover = true;
+            	    for (Obstaculo o : obstaculos) {
+            	        if (player.chequeoColisionX(-3, o)) {
+            	            puede_mover = false;
+            	            break;
+            	        }
+            	    }
+            	    if (puede_mover) {
+            	        // Si Mario todavía no llegó al centro de la pantalla o el mundo ya está en el inicio
+            	        if (player.getX() > getWidth() / 2 || worldOffset <= 0) {
+            	            player.moverIzquierda(3);
+            	        } else {
+            	            // Desplazamos el mundo a la derecha
+            	            worldOffset -= 3;
+            	            fondoPanel.desplazamiento = worldOffset;
+            	            for (Obstaculo o : new ArrayList<>(obstaculos)) {
+            	                o.setLocation(o.getX() + 3, o.getY()); // mover obstáculos para la derecha.
+            	            }
+            	            for (Enemigo enemigo : new ArrayList<>(enemigos)) {
+            	            	if (!enemigo.isFijoEnPantalla()) {
+            	            	enemigo.setLocation(enemigo.getX() + 3, enemigo.getY()); 
+            	            	enemigo.ajustarLimites(3); // si el mundo se mueve a la derecha (A)
+            	            	}
+            	            	/*
+            	            	else {
+            	            		enemigo.setLocation(enemigo.getX() - 3, enemigo.getY()); 
+                	            	enemigo.ajustarLimites(-3);
+            	            	}
+            	            	*/
+            	            }
+            	            fondoPanel.repaint();
+            	        }
+            	    }
+            	}
+
+                
+                
+            	if (dPressed) {                	
+                	if (iteracion == 1) {
+            			URL url = getClass().getResource("/img/personajes/MARIO_corriendo4.png");
+            			if (url != null) {
+            			    player.setIcon(new ImageIcon(url));
+            			} else {
+            			    System.err.println("No se encontró la imagen: MARIO_corriendo4.png");
+            			}
+            		}
+            		if (iteracion != 1) {
+            			URL url = getClass().getResource("/img/personajes/mario_corriendo5.png");
+            			if (url != null) {
+            			    player.setIcon(new ImageIcon(url));
+            			} else {
+            			    System.err.println("No se encontró la imagen: mario_corriendo5.png");
+            			}
+            		}
+            		
+                    boolean puede_mover = true;
+                    for (Obstaculo o : new ArrayList<>(obstaculos)) {
+                        if (player.chequeoColisionX(3, o)) {
+                            puede_mover = false;
+                            break;
+                        }
+                    }
+                    if (puede_mover) {
+                        // Si Mario está en la primera mitad de la pantalla o ya llegamos al final del mapa
+                        if (player.getX() < getWidth() / 2 || worldOffset >= anchoMapa - getWidth()) {
+                            player.moverDerecha(contentPane.getWidth(), 3);
+                        } else {
+                            // Se desplaza el mundo a la izquierda.
+                            worldOffset += 3;
+                            fondoPanel.desplazamiento = worldOffset;
+                            for (Obstaculo o : new ArrayList<>(obstaculos)) {
+                                o.setLocation(o.getX() - 3, o.getY()); // mover obstáculos para la izquierda.
+                            }
+                            for (Enemigo enemigo : new ArrayList<>(enemigos)) {
+                            	if (!enemigo.isFijoEnPantalla()) {
+                            	enemigo.setLocation(enemigo.getX() - 3, enemigo.getY());
+                            	enemigo.ajustarLimites(-3); // si el mundo se mueve a la izquierda (D)
+                            	}
+                            	
+            	            	
+                            }
+                            fondoPanel.repaint();
+                        }
+                    }
+                }
+
+                if (wPressed) {
+                    player.saltar();
+                    wPressed = false;
+                }
+                
+                int posicionJugador = worldOffset + player.getX();
+                if (posicionJugador >= 3000 && !nivelSuperado) {
+                    nivelSuperado = true; 				// evita que se vuelva a superar el nivel.
+                    fondoPanel.setearNivelSuperado(true);
+                    repaint();
+                    System.out.println("Nivel superado!");
+                    
+                    aPressed = false;
+                    dPressed = true;
+                    Sonido.reproducirEfecto("sonidos/player/superarNivel.wav"); 
+                    new Timer(1750, new ActionListener() {
+                        public void actionPerformed(ActionEvent evt) {
+                            ((Timer) evt.getSource()).stop();
+                            GestorNiveles.avanzarNivel(NivelBase.this);
+                        }
+                    }).start();
+                }
+                
+            }
+                        
+        });
+        movimientoFluido.start();
+    }
+    
+    public void mostrarPantallaGameOver() {
+        if (movimientoFluido != null) movimientoFluido.stop();
+        for (Enemigo enemigo : enemigos) enemigo.detenerPatrulla();
+        if (player != null) player.detenerTimers();
+
+        PantallaGameOver gameOver = new PantallaGameOver();
+        setContentPane(gameOver);
+        revalidate();
+        repaint();
+    }
+
+    
+    
+    
+    public void cerrarNivel() {
+    	 if (movimientoFluido != null) {
+    	        movimientoFluido.stop();
+    	    }
+    	    for (Enemigo enemigo : enemigos) {
+    	        enemigo.detenerPatrulla();
+    	    }
+    	    if (player != null) {
+    	        player.detenerTimers();
+    	    }
+
+    	    // Limpieza de todo para que no quede ningún objeto residual.
+    	    contentPane.removeAll();
+    	    obstaculos.clear();
+    	    enemigos.clear();
+
+    	    dispose();
+    }
+   
+}
